39a26b3b23ba96175ffa9ad43044ef75d0f98218
greenhat616 <i@a632079.me>
2022-11-20 02:27:01 +0800

    build

---
+++ dist/types/src/docs-builder.d.ts
@@ -0,0 +1,9 @@
+import { Options } from "./types";
+interface Doc {
+    id: string;
+    link: string;
+    b: string;
+    a: string;
+}
+declare const buildDocs: (HTML_FOLDER: string, options: Options) => Promise<Doc[]>;
+export default buildDocs;
